9756f95e80f3169d530395d5fa2bed47dfb26482
Daniel Avelar <danielavelar@Daniels-MacBook-Pro.local>
2023-06-09 14:42:44 -0400

    added if logic in server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,13 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
